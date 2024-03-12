0a1420deb3a3dd3e4c71dd8b3d9e2e8991425b06
Osama <osama.altaf385@gmail.com>
2024-03-13 00:07:59 +0500

    node backend application

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import userRoutes from "./routes/users.js";
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use("/users", userRoutes);
+
+app.get("/", (req, res) => {
+  console.log("[GET ROUTE]");
+  res.send("HELLO FROM HOMEPAGE");
+});
+
+// app.get("/", (req, res));
+
+// app.get("/users", (req, res) => {
+//   console.log("[GET ROUTE]");
+//   res.send("List of Users");
+// });
+
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`)
+);
